d3f40d7ae7f5a14df8c451cb7ba4b660e40c4cbe
Karsten Sønderup <ks@kaso.dk>
2024-12-05 14:35:44 +0100

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Kaso",
+  title: "1S",
   description: "This domain is currently parked. Stay tuned for something amazing!",
 };
 
