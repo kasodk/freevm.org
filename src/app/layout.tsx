76da338f350a14a4fc97774c4fbe1e84680c996a
Karsten Sønderup <ks@kaso.dk>
2024-12-13 18:48:07 +0100

    Port changed to 3005

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "ByteStash",
+  title: "CliMX",
   description: "This domain is currently parked. Stay tuned for something amazing!",
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const Home: React.FC = () => {
     <main className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 flex items-center justify-center text-white">
       <div className="text-center max-w-lg px-4">
         <h1 className="text-5xl font-bold mb-4">
-          Welcome to bytestash.net.
+          Welcome to climx.net.
         </h1>
         <p className="text-lg">
           This domain is currently parked. Stay tuned for something amazing!
