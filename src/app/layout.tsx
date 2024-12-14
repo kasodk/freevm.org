4f1226a35d44f7a81f0b984283e7df045535d558
Karsten Sønderup <ks@kaso.dk>
2024-12-14 18:05:13 +0100

    Port changed to 3008

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Find VPN",
+  title: "FreeVM",
   description: "This domain is currently parked. Stay tuned for something amazing!",
 };
 
